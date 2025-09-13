699612fe6b781c53b414764fbfcc1c7f53b18508
Vladimir Romanov <vladimirromanov@me.com>
2025-09-13 09:07:46 +0300

    rect

--- Day_03/Homework/hw_rect_magic_methods.py
+++ Day_03/Homework/hw_rect_magic_methods.py
@@ -1,42 +1,42 @@
 class Rect:
-    def __init__(self, width, height):
+    def __init__(self, width=1, height=1):
         self.width = width
         self.height = height
 
     def __repr__(self):
-        return f'Rect(width:{self.width}, height:{self.height})'
+        return f'Rect({self.width}, {self.height})'
 
     def __str__(self):
         return f'Value width is {self.width}, value height in {self.height}'
 
-    def __mul__(self, other):
+    def __mul__(self, other: int | float):
         try:
             if isinstance(other, (int, float)):
                 self.width *= other
                 self.height *= other
         except:
-            ValueError
+            TypeError('Bad Type')
 
     def get_area(self):
         return self.width * self.height
 
     def __lt__(self, other):
-        return self.get_area() < Rect.get_area(other)
+        return self.get_area() < other.get_area()
 
     def __gt__(self, other):
-        return self.get_area() > Rect.get_area(other)
+        return self.get_area() > other.get_area()
 
     def __le__(self, other):
-        return self.get_area() <= Rect.get_area(other)
+        return self.get_area() <= other.get_area()
 
     def __ge__(self, other):
-        return self.get_area() >= Rect.get_area(other)
+        return self.get_area() >= other.get_area()
 
     def __eq__(self, other):
-        return self.get_area() == Rect.get_area(other)
+        return self.get_area() == other.get_area()
 
     def __ne__(self, other):
-        return self.get_area() != Rect.get_area(other)
+        return self.get_area() != other.get_area()
 
 
 r1 = Rect(2, 23)
